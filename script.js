5b66c644a75e2c9d7742cdd08667252d29388c3b
skyekoh <skyekoh2@gmail.com>
2024-07-30 15:46:56 -0600

    added carousel and mobile optimization for the about section

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let index = 0;
+  const items = document.querySelectorAll('.carousel-item');
+  const totalItems = items.length;
+
+  function showNextItem() {
+      items[index].classList.remove('active');
+      index = (index + 1) % totalItems;
+      items[index].classList.add('active');
+  }
+
+  function startCarousel() {
+      setInterval(showNextItem, 3000); // Change slide every 3 seconds
+  }
+
+  startCarousel();
+});
